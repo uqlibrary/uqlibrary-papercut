b5c197ee6585dcb0f37e33c8a11b38e78777c276
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-01-10 12:32:26 +1000

    Update test scripts

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-echo "Testing branch: ${CI_BRANCH} with pipe ${PIPE_NUM}"
+echo "Testing branch: ${CI_BRANCH}"
+if [ ${CI_BRANCH} != "gh-pages" ]; then
 
-if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
-    # Run local tests
-    echo "Installing global"
-    npm install -g bower web-component-tester
-
-    echo "Installing bower dependencies"
-    bower install
-
+if [ ${PIPE_NUM} == "1" ]; then
     echo "Starting local WCT tests"
-    wct
-else
-    echo "Pipe not used."
+    npm test
+fi
+
 fi
